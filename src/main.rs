8885b82d707731508602e382ca76189d362ded0e
Koji Ota <afterjnih@gmail.com>
2016-03-03 09:23:04 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+extern crate mio;
+
+use mio::tcp::*;
+
+const SERVER: mio::Token = mio::Token(0);
+
+struct Pong {
+    server: TcpListener,
+}
+
+impl mio::Handler for Pong {
+    type Timeout = ();
+    type Message = ();
+}
+
+fn main() {
+    let address = "0.0.0.0:6567".parse().unwrap();
+    let server = TcpListener::bind(&address).unwrap();
+
+    let mut event_loop = mio::EventLoop::new().unwrap();
+    event_loop.register(&server, SERVER);
+
+    println!("running pingpong server");
+    event_loop.run(&mut Pong { server: server });
+}
