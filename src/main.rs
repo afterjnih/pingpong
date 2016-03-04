418c6db3ff3408172917d1ba3a913cdcd68265c3
Koji Ota <afterjnih@gmail.com>
2016-03-04 09:22:44 +0900

    update Pong struct

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,38 @@ const SERVER: mio::Token = mio::Token(0);
 
 struct Pong {
     server: TcpListener,
+    connections: Slab<Connection>,
 }
 
 impl mio::Handler for Pong {
     type Timeout = ();
     type Message = ();
+
+    fn ready(&mut self,
+             event_loop: &mio::EventLoop<Pong>,
+             token: mio::Token,
+             events: mio::EventSet) {
+        match token {
+            SERVER => {
+                assert!(events.is_readable());
+                println!("this server socket is ready to accept a connection");
+                match self.server.accept() {
+                    Ok(Some(connection)) => {
+                        println!("accepted a socket, exiting program");
+                        event_loop.shutdown();
+                    }
+                    Ok(None) => {
+                        println!("the server socket wasn't actually ready");
+                    }
+                    Err(e) => {
+                        println!("listener.accept() errored: {}", e);
+                        event_loop.shutdown();
+                    }
+                }
+            }
+            _ => panic!("Received unknown token"),
+        }
+    }
 }
 
 fn main() {
